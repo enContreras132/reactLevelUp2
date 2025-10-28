94547b382b1213e717cc1f95f9a4e2254ad248e6
Fabianxarii <fa.cuevast@duocuc.cl>
2025-10-28 19:48:50 -0300

    Componente sobre nosotros
    creacion y arreglo de dicho componente

---
+++ src/components/SobreNosotros.jsx
@@ -0,0 +1,32 @@
+function SobreNosotros() {
+  return (
+    <div>
+      {/* about section */}
+      <section className="about_section layout_padding">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="img-box">
+                <img src="https://www.levelupvlc.com/_next/image?url=https%3A%2F%2Fhebbkx1anhila5yf.public.blob.vercel-storage.com%2Flogo-portrait-ycwjxbJNaPbJdVE0WBtXX0TDDC1DMg.webp&w=256&q=75" alt="" />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="detail-box">
+                <div className="heading_container">
+                  <h2>Sobre Level Up</h2>
+                </div>
+                <p>
+                  Level-Up Gamer es una tienda online dedicada a satisfacer las necesidades de los entusiastas de los videojuegos en Chile. Lanzada hace dos años como respuesta a la creciente demanda durante la pandemia, Leves-Up Gamer ofrece una amplia gama de productos para gamers, desde consolas y accesorios hasta computadores y sillas especializadas. Aunque no cuenta con una ubicación física, realiza despachos a todo el país.
+                </p>
+                <a href="#">Leer Mas</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* end about section */}
+    </div>
+  );
+}
+
+export default SobreNosotros
