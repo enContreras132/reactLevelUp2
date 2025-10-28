5cb00c442aee58a8e1594cafb09b9c12a467c1b0
Fabianxarii <fa.cuevast@duocuc.cl>
2025-10-28 20:18:25 -0300

    Componente Separador
    creacion del componente Separador
    y actualizacion de este

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import SobreNosotros from './components/SobreNosotros'
+import Separador from './components/Separador'
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<><HomePage /><Separador /></>} />
         <Route path="/sobrenosotros" element={<SobreNosotros />} />
       </Routes>
       <Footer />
