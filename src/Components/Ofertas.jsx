32e96025642dc4a28fec2b516b6fa741142c3e4f
Enzo <en.contrerase@duocuc.cl>
2025-10-28 19:58:11 -0300

    componente carrusel y oferta

---
+++ src/Components/Ofertas.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Ofertas = () => {
+  return (
+    <section className="offer_section layout_padding-bottom">
+      <div className="offer_container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="box">
+                <div className="img-box">
+                  <img src="/assets/images/logolevelup (1).png" alt="Oferta Duocuc" />
+                </div>
+                <div className="detail-box">
+                  <h5>Ofertas Por Registro con Correo de Duocuc</h5>
+                  <h6>
+                    <span>20%</span> Off
+                  </h6>
+                  <Link to="/productos">
+                    Compra Ahora{" "}
+                    <svg
+                      version="1.1"
+                      id="Capa_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      x="0px"
+                      y="0px"
+                      viewBox="0 0 456.029 456.029"
+                      style={{ enableBackground: "new 0 0 456.029 456.029" }}
+                      xmlSpace="preserve"
+                    ></svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="box">
+                <div className="img-box">
+                  <img src="/assets/images/logolevelup (1).png" alt="Oferta Fin de Semana" />
+                </div>
+                <div className="detail-box">
+                  <h5>Ofertas Fin de Semana</h5>
+                  <h6>
+                    <span>15%</span> Off
+                  </h6>
+                  <Link to="/productos">
+                    Compra Ahora{" "}
+                    <svg
+                      version="1.1"
+                      id="Capa_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      x="0px"
+                      y="0px"
+                      viewBox="0 0 456.029 456.029"
+                      style={{ enableBackground: "new 0 0 456.029 456.029" }}
+                      xmlSpace="preserve"
+                    ></svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Ofertas;
