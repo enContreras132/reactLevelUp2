d34020075e7a6025c2bd0a171b6883ee7e0e0a29
MaxiKresse <en.contrerase@duocuc.cl>
2025-12-01 19:12:48 -0300

    Rebombrar temporalmente

---
+++ src/Components/Separador.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+function Separador() {
+    return (
+        <>
+            {/*separador visual*/}
+            <div className="container-fluid my-5">
+                <div className="row align-items-center">
+                    <div className="col-md-7 ">
+                        <img src="https://images.unsplash.com/photo-1548092372-0d1bd40894a3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170" alt="" className="img-fluid" />
+                    </div>
+                    <div className="col-md-5 ">
+                        <h3 className="bbh-sans-bartle-regular mb-3">Los mejores productos</h3>
+                        <h3 className="bbh-sans-bartle-regular mb-3">Los mejores productos</h3>
+                        <h3 className="bbh-sans-bartle-regular mb-3">Los mejores productos</h3>
+                        <h3 className="bbh-sans-bartle-regular mb-3">Al mejor precio</h3>
+                        <h3 className="bbh-sans-bartle-regular mb-3">Al mejor precio</h3>
+                        <h3 className="bbh-sans-bartle-regular mb-3">Al mejor precio</h3>
+                        <h3 className="bbh-sans-bartle-regular mb-3">Los mejores productos</h3>
+                        <h3 className="bbh-sans-bartle-regular mb-3">Los mejores productos</h3>
+                        <h3 className="bbh-sans-bartle-regular mb-3">Los mejores productos</h3>
+                    </div>
+                </div>
+            </div>
+            {/*fin separador visual*/}
+        </>
+    );
+}
+export default Separador;
